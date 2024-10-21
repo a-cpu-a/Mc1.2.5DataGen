1fdbb878374f86c66791f8eb6a7e7b254be064ed
a-cpu-a <54659787+a-cpu-a@users.noreply.github.com>
2024-10-21 10:57:30 +0300

    Create it

---
+++ src/main/java/net/minecraft/src/mod_DataGen.java
@@ -0,0 +1,10 @@
+package net.minecraft.src;
+
+public class mod_DataGen extends BaseMod{
+    public void load() {
+        System.out.println("Hello world! (DATA GEN)");
+    }
+    public String getVersion() {
+        return "1.0";
+    }
+}
